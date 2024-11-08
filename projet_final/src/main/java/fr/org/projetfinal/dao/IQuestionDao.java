c6ce9e437bc06ff6bf7085ad920e56e46c5601e9
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-08 11:34:49 +0100

    Model(Bean) Question et la Dao pour la table question crée

---
+++ projet_final/src/main/java/fr/org/projetfinal/dao/IQuestionDao.java
@@ -0,0 +1,11 @@
+package fr.org.projetfinal.dao;
+
+import java.util.List;
+
+import fr.org.projetfinal.model.Question;
+
+public interface IQuestionDao {
+	
+	List<Question> getAllQuestions();
+
+}
