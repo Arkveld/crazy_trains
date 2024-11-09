9325909c5d2d39672bb18700a3f009f7dd36e23b
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-09 11:57:23 +0100

    Méthodes de cryptage crée

--- projet_final/src/main/java/fr/org/projetfinal/security/AlgoCryptage.java
+++ projet_final/src/main/java/fr/org/projetfinal/security/AlgoCryptage.java
@@ -1,5 +1,23 @@
 package fr.org.projetfinal.security;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
+import javax.crypto.Cipher;
+
 public abstract class AlgoCryptage {
 
+	
+	//Cryptage
+	public static byte[] encrypt(String passwordToEncrypt, Key key, String algorithme) throws Exception {
+		
+		//Cipher
+		Cipher cipher = Cipher.getInstance(algorithme);
+		
+		//Initialisation
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		
+		byte[] passwordCrypted = cipher.doFinal(passwordToEncrypt.getBytes(StandardCharsets.UTF_8));
+		return passwordCrypted;
+	}
 }
--- projet_final/src/main/java/fr/org/projetfinal/security/GenerateKey.java
+++ projet_final/src/main/java/fr/org/projetfinal/security/GenerateKey.java
@@ -1,6 +1,27 @@
 package fr.org.projetfinal.security;
 
+import java.security.Key;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public abstract class GenerateKey {
 
-	
+	public static Key getkey(String algorithme, int keySize) throws Exception {
+		
+		//Générateur de Key
+		KeyGenerator keyGen = KeyGenerator.getInstance(algorithme);
+		//Initialise la clé
+		keyGen.init(keySize);
+		
+		//Crée la clé secrète
+		SecretKey secretKey = keyGen.generateKey();
+		
+		//Obtention de la clé
+		Key key = new SecretKeySpec(secretKey.getEncoded(), algorithme);
+		return key;
+		
+		
+	}
 }
