0a61a69aa5f73554e38116a8114a9b43b591d699
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-09 12:11:23 +0100

    Cryptage du mot de passe ajouté dans la DAO

--- projet_final/src/main/java/fr/org/projetfinal/dao/UserDaoImp.java
+++ projet_final/src/main/java/fr/org/projetfinal/dao/UserDaoImp.java
@@ -1,10 +1,13 @@
 package fr.org.projetfinal.dao;
 
+import java.security.Key;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 import fr.org.projetfinal.bdd.MyConnectionSQL;
 import fr.org.projetfinal.model.User;
+import fr.org.projetfinal.security.AlgoCryptage;
+import fr.org.projetfinal.security.GenerateKey;
 
 public class UserDaoImp implements IUserDao {
 	
@@ -16,24 +19,36 @@ public class UserDaoImp implements IUserDao {
 		//Connexion
 		this.connection = MyConnectionSQL.getInstance();
 		
-		//PrepareStatement
-		PreparedStatement prepareStatement = connection.prepareStatement("INSERT INTO user (nom, prenom, mail, password, role, question_id, reponse) VALUES (?, ?, ?, ?, ?, ?, ?)");
-		prepareStatement.setString(1, user.getNom());
-		prepareStatement.setString(2, user.getPrenom());
-		prepareStatement.setString(3, user.getEmail());
-		//Cryptage
+		try {
+			
+			//PrepareStatement
+			PreparedStatement prepareStatement = connection.prepareStatement("INSERT INTO user (nom, prenom, mail, password, role, question_id, reponse) VALUES (?, ?, ?, ?, ?, ?, ?)");
+			prepareStatement.setString(1, user.getNom());
+			prepareStatement.setString(2, user.getPrenom());
+			prepareStatement.setString(3, user.getEmail());
+			
+			
+			//Cryptage du mot de passe
+			Key key = GenerateKey.getkey("DES", 56);
+			byte[] passwordCrypted = AlgoCryptage.encrypt(user.getPassword(), key, "DES");
+			prepareStatement.setBytes(4, passwordCrypted);
+			
+			prepareStatement.setString(5, user.getRole());
+			prepareStatement.setInt(6, user.getQuestion_id());
+			prepareStatement.setString(7, user.getRole());
+			
+			prepareStatement.executeUpdate();
+			
+			prepareStatement.close();
+			connection.close();
+			
+		} catch(Exception e) {
+			
+		}
 		
-		//prepareStatement.setBytes(4, user.getPassword().getBytes());
-		
-		prepareStatement.setString(5, user.getRole());
-		prepareStatement.setInt(6, user.getQuestion_id());
-		prepareStatement.setString(7, user.getRole());
-		
-		prepareStatement.executeUpdate();
 		
 	
-		prepareStatement.close();
-		connection.close();
+		
 		
 		return user;
 	}
