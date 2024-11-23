77494dc556d50aea27d5ce66d917564a8c5941f9
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-23 10:16:38 +0100

    Model et table Articles crée

---
+++ projet_final/src/main/java/fr/org/projetfinal/model/Article.java
@@ -0,0 +1,65 @@
+package fr.org.projetfinal.model;
+
+public class Article {
+
+	private int id;
+	private String titre;
+	private String contenu;
+	private String contenuNoSql;
+	private String date;
+	private int user_id;
+
+	public Article() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+
+	public String getContenuNoSql() {
+		return contenuNoSql;
+	}
+
+	public void setContenuNoSql(String contenuNoSql) {
+		this.contenuNoSql = contenuNoSql;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+
+}
