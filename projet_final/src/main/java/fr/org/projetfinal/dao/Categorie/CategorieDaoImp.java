c244a07c1b5c7af92536f658118d2541dfa2c683
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-24 21:29:15 +0100

    Dao Catégorie (liste crée)

--- projet_final/src/main/java/fr/org/projetfinal/dao/Categorie/CategorieDaoImp.java
+++ projet_final/src/main/java/fr/org/projetfinal/dao/Categorie/CategorieDaoImp.java
@@ -1,5 +1,41 @@
 package fr.org.projetfinal.dao.Categorie;
 
-public class CategorieDaoImp {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.org.projetfinal.bdd.MyConnectionSQL;
+import fr.org.projetfinal.model.Categorie;
+
+public class CategorieDaoImp implements ICategorieDao {
+	
+	private Connection connection;
+
+	@Override
+	public List<Categorie> getAllCategories() throws Exception {
+		
+		List<Categorie> categories = new ArrayList<Categorie>();
+		
+		this.connection = MyConnectionSQL.getInstance();
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM categorie");
+		
+		while(resultSet.next()) {
+			
+			Categorie categorie = new Categorie();
+			categorie.setId(resultSet.getInt("id"));
+			categorie.setNom_categorie(resultSet.getString("nom_categorie"));
+			
+			categories.add(categorie);
+		}
+		
+		resultSet.close();
+		statement.close();
+		connection.close();
+		
+		return categories;
+	}
 
 }
--- projet_final/src/main/java/fr/org/projetfinal/dao/Categorie/ICategorieDao.java
+++ projet_final/src/main/java/fr/org/projetfinal/dao/Categorie/ICategorieDao.java
@@ -1,5 +1,10 @@
 package fr.org.projetfinal.dao.Categorie;
 
+import java.util.List;
+
+import fr.org.projetfinal.model.Categorie;
+
 public interface ICategorieDao {
 
+	public List<Categorie> getAllCategories() throws Exception;
 }
