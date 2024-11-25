68b28577353224c2116f478f9106060200c28453
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-25 11:35:05 +0100

    Servet et page pour la publication crée

---
+++ projet_final/WebContent/articles/saveArticle.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> Publication</title>
+</head>
+<body>
+	<h1> Ajouter un article</h1>
+	<form >
+		<div>
+			<label for="titre"> Votre titre </label>
+			<input type="text"/>
+		</div>
+		<input type="submit" value="Pubilier">
+	</form>
+</body>
+</html>
---
+++ projet_final/src/main/java/fr/org/projetfinal/controller/Publish.java
@@ -0,0 +1,19 @@
+package fr.org.projetfinal.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="/Publish")
+public class Publish extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/articles/saveArticle.jsp").forward(request, response);
+	}
+
+}
