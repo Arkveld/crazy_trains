78040400c01b8849ba41a9449818473d4c3c132f
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-25 11:24:04 +0100

    Categorie dans la couche métier implémenté

--- projet_final/src/main/java/fr/org/projetfinal/metier/CategorieMetierImp.java
+++ projet_final/src/main/java/fr/org/projetfinal/metier/CategorieMetierImp.java
@@ -1,5 +1,19 @@
 package fr.org.projetfinal.metier;
 
-public class CategorieMetierImp {
+import java.util.List;
+
+import fr.org.projetfinal.dao.Categorie.CategorieDaoImp;
+import fr.org.projetfinal.dao.Categorie.ICategorieDao;
+import fr.org.projetfinal.model.Categorie;
+
+public class CategorieMetierImp implements ICategorieMetier {
+
+	private ICategorieDao categorieDao;
+	@Override
+	public List<Categorie> findAllCategories() throws Exception {
+		this.categorieDao = new CategorieDaoImp();
+		return categorieDao.getAllCategories();
+	}
+	
 
 }
--- projet_final/src/main/java/fr/org/projetfinal/metier/ICategorieMetier.java
+++ projet_final/src/main/java/fr/org/projetfinal/metier/ICategorieMetier.java
@@ -1,5 +1,10 @@
 package fr.org.projetfinal.metier;
 
+import java.util.List;
+
+import fr.org.projetfinal.model.Categorie;
+
 public interface ICategorieMetier {
 
+	public List<Categorie> findAllCategories() throws Exception;
 }
