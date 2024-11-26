fa3e038c2b897ed4ea3f0b4f7eaedeeb2903a12b
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-26 17:04:46 +0100

    Servlet et page articles crée

---
+++ projet_final/src/main/java/fr/org/projetfinal/controller/Articles.java
@@ -0,0 +1,21 @@
+package fr.org.projetfinal.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "/Articles")
+public class Articles extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("articles/articles.jsp").forward(request, response);
+	}
+
+}
