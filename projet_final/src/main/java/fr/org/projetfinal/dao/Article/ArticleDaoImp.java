64eb082cf2860b732896dce35ab67893a75d847a
Michael MABONZO <michael.mabonzo@gmail.com>
2024-11-26 14:54:58 +0100

    Methode pour obtenir un article selon un id crée

--- projet_final/src/main/java/fr/org/projetfinal/dao/Article/ArticleDaoImp.java
+++ projet_final/src/main/java/fr/org/projetfinal/dao/Article/ArticleDaoImp.java
@@ -4,12 +4,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 import org.bson.Document;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 import fr.org.projetfinal.bdd.MongoConnection;
 import fr.org.projetfinal.bdd.MyConnectionSQL;
@@ -19,7 +25,10 @@ public class ArticleDaoImp implements IArticleDao {
 
 	private Connection connection;
 	private MongoClient mongoConnection;
+	
 	@Override
+	/********** Ajouter un article ****************/
+	
 	public Article addArticle(Article article) throws Exception {
 		
 		//id
@@ -55,7 +64,7 @@ public class ArticleDaoImp implements IArticleDao {
 			Document document = new Document("id", id).append("url", article.getImageUrl()).append("legende", article.getLegende());
 			collection.insertOne(document);
 			
-			//On ferme la seesion
+			//On ferme la session
 			prepareStatement.close();
 			mongoConnection.close();
 			connection.close();
@@ -66,6 +75,85 @@ public class ArticleDaoImp implements IArticleDao {
 		
 		return article;
 		
+	}
+	
+	@Override
+	/**************Obtenir tous les articles ********************/
+	
+	public List<Article> getArticles() throws Exception {
+		List<Article> articles = new ArrayList<Article>();
+		//MySql
+		this.connection = MyConnectionSQL.getInstance();
+		try {
+			String query = "SELECT * FROM articles";
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(query);
+			
+			while(rs.next()) {
+				
+				Article article = new Article();
+				article.setId(rs.getInt("id"));
+				article.setTitre(rs.getString("titre"));
+				article.setContenu(rs.getString("contenu"));
+				article.setDate(rs.getString("date"));
+				
+				articles.add(article);
+				
+				//On ferme la session
+				rs.close();
+				statement.close();
+				connection.close();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return articles;
+	}
+
+	@Override
+	
+	/************ Obtenir un article selon son id *************/
+	
+	public Article getArticleById(int id) throws Exception {
+		Article article = new Article();
+		
+		this.connection = MyConnectionSQL.getInstance();
+		this.mongoConnection = MongoConnection.getInstance();
+		
+		try {
+			String query = "SELECT * FROM articles WHERE id = ?";
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				article.setId(rs.getInt("id"));
+				article.setTitre(rs.getString("titre"));
+				article.setContenu(rs.getString("contenu"));
+				article.setDate(rs.getString("date"));
+			}
+			
+			rs.close();
+			ps.close();
+			connection.close();
+			
+			MongoDatabase database = mongoConnection.getDatabase("train");
+			MongoCollection<Document> collection = database.getCollection("articles");
+			MongoCursor<Document> cursor = collection.find(Filters.eq("id", id)).iterator();
+			
+			while(cursor.hasNext()) {
+				article.setImageUrl(cursor.next().getString("url"));
+				article.setLegende(cursor.next().getString("legende"));
+			}
+			
+			mongoConnection.close();
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return article;
 	}
 	
 	
--- projet_final/src/main/java/fr/org/projetfinal/dao/Article/IArticleDao.java
+++ projet_final/src/main/java/fr/org/projetfinal/dao/Article/IArticleDao.java
@@ -1,8 +1,12 @@
 package fr.org.projetfinal.dao.Article;
 
+import java.util.List;
+
 import fr.org.projetfinal.model.Article;
 
 public interface IArticleDao {
 
 	public Article addArticle(Article article) throws Exception;
+	public List<Article> getArticles() throws Exception;
+	public Article getArticleById(int id) throws Exception;
 }
